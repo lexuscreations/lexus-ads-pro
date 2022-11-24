67fbd285ce0d0105dbea1b0c3e1483d512eadb6b
LokeshVishwakarma <72046165+lexuscreations@users.noreply.github.com>
2022-11-25 02:12:09 +0530

    Add files via upload

--- LexusAdSkipper/script.js
+++ LexusAdSkipper/script.js
@@ -1,21 +1,94 @@
-setInterval(() => {
-  let ytMdiaPlyScrnEleDivSec = document.querySelector("#player-container-outer.style-scope.ytd-watch-flexy")
-  if (ytMdiaPlyScrnEleDivSec != undefined) {
-    if (document.querySelector(".lexusVideoTopStatusAppIndicator") == undefined) {
-      let lexusStatusIndicatorElement = document.createElement("h2")
-      lexusStatusIndicatorElement.textContent = `LexusAdSkipper: Activated`
-      lexusStatusIndicatorElement.classList.add("lexusVideoTopStatusAppIndicator")
-      ytMdiaPlyScrnEleDivSec.prepend(lexusStatusIndicatorElement)
-    }
-  }
-  let skipButtonLexusVideo = document.getElementsByClassName("ytp-ad-skip-button")
-  let skipButtonLexusTextAd = document.getElementsByClassName("ytp-ad-overlay-close-button")
-  if (skipButtonLexusVideo != undefined && skipButtonLexusVideo.length > 0) {
-    console.log("LexusAdSkipper: VideoAdSkipped")
-    skipButtonLexusVideo[0].click()
-  }
-  if (skipButtonLexusTextAd != undefined && skipButtonLexusTextAd.length > 0) {
-    console.log("LexusAdSkipper: TextAdSkipped")
-    skipButtonLexusTextAd[0].click()
-  }
-}, 3000)
+(() => {
+  if (new URL(location.href).origin !== "https://www.youtube.com") return;
+
+  let mainInterval;
+  const appName = "LexusAdSkipper";
+  const skipButtonVideoSelector = ".ytp-ad-skip-button";
+  const skipButtonTextSelector = ".ytp-ad-overlay-close-button";
+  const playerContainerSelector =
+    "#player-container-outer.style-scope.ytd-watch-flexy";
+  const cinematicBlurSelector = "#cinematics.ytd-watch-flexy";
+
+  setInterval(() => {
+    const playerContainerEl = document.querySelector(playerContainerSelector);
+
+    if (!playerContainerEl) return mainInterval && clearInterval(mainInterval);
+
+    mainInterval = setInterval(() => {
+      const skipButtonVideoEl = document.querySelector(skipButtonVideoSelector);
+      const skipButtonTextEl = document.querySelector(skipButtonTextSelector);
+      const cinematicBlurEl = document.querySelector(cinematicBlurSelector);
+      const appStatusEl = document.querySelector(
+        `.${appName}StatusAppIndicator`
+      );
+
+      if (cinematicBlurEl) {
+        cinematicBlurEl.style.display = "none";
+      }
+
+      if (!appStatusEl) {
+        const initialTextForLoopBtn = document.querySelector(
+          "video.video-stream.html5-main-video"
+        ).loop
+          ? "Loop - on"
+          : "Loop - off";
+        playerContainerEl.insertAdjacentHTML(
+          "afterbegin",
+          `<div style="margin-bottom: 5px;display: flex;justify-content: flex-start;align-items: center;" class="${appName}StatusAppIndicator">
+            <h2 style="color: white;margin-right: 22px;">${appName}: Activated</h2>
+            <button id="${appName}LoopBtn" style="cursor: pointer;margin-right: 22px;background: white;" onclick="
+              ((thisEl) => {
+                const videoEl = document.querySelector('video.video-stream.html5-main-video');
+                if (videoEl.loop) {
+                  thisEl.textContent = 'Loop - off';
+                  thisEl.style.backgroundColor = 'white';
+                  videoEl.loop = false;
+                } else {
+                  thisEl.textContent = 'Loop - on';
+                  thisEl.style.backgroundColor = 'green';
+                  videoEl.loop = true;
+                }
+              })(this)
+            ">${initialTextForLoopBtn}</button>
+            <button id="${appName}PictureInPlayBtn" style="cursor: pointer;background: white;" onclick="
+              (async (thisEl) => {
+                const videoEl = document.querySelector('video.video-stream.html5-main-video');
+                await videoEl.requestPictureInPicture();
+                thisEl.style.backgroundColor = 'green';
+                videoEl.onleavepictureinpicture = () => {
+                  thisEl.style.backgroundColor = 'white';
+                }
+              })(this)
+            ">PictureInPlay</button>
+          </div>`
+        );
+      } else {
+        const videoEl = document.querySelector(
+          "video.video-stream.html5-main-video"
+        );
+        const loopBtnEl = document.querySelector(
+          `.${appName}StatusAppIndicator #${appName}LoopBtn`
+        );
+        if (videoEl && loopBtnEl) {
+          if (videoEl.loop) {
+            loopBtnEl.textContent = "Loop - on";
+            loopBtnEl.style.backgroundColor = "green";
+          } else {
+            loopBtnEl.textContent = "Loop - off";
+            loopBtnEl.style.backgroundColor = "white";
+          }
+        }
+      }
+
+      if (skipButtonVideoEl) {
+        console.log(`${appName}: VideoAdSkipped`);
+        skipButtonVideoEl.click();
+      }
+
+      if (skipButtonTextEl) {
+        console.log(`${appName}: TextAdSkipped`);
+        skipButtonTextEl.click();
+      }
+    }, 3000);
+  }, 3000);
+})();
